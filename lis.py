0864ed7fbb165f7d29c27f79e314dcbcc41eccf0
j4der <jaderandres3007@gmail.com>
2025-12-23 15:48:39 -0500

    escala 1
    se aumento las capacidades de la lista, ahora tiene un menu donde agrega una variedad de opciones

--- lis.py
+++ lis.py
@@ -6,14 +6,31 @@ print (f"Hoy es {fecha}  asi que compra la cena navideña 🎅")
 print ("Cuando acabes escribe:  listo ")
 
 while True:
-   comp= input("comprar: ")
-
-   if comp.lower() == "listo":
-      break
+   print ("1 Crear lista de compras")
+   print ("2 añadir productos a tu lista")
+   print ("3 crear nueva lista")
+   print ("4 salir")
+   opc= input(":")
    
-   lista_c.append(comp)
+   if opc == 1:
+       print ("Escribe listo cuando termines")
+       while True:
+           comp= input()
+           lista_c.append(comp)
+           if comp == "listo":
+               break
+       break
+    elif opc == 2:
+        if not lista_c:
+            print("No hay lista creada aún.")
+            continue
+   elif opc == 3:
+       lista_c = []
+   elif opc == 4:
+       break
 
 print ("\n Lista de compras cena navideña") 
-for i, comp in enumerate (comp, start=1):
-    
+for i, comp in enumerate (lista_c, start=1):
     print (i, comp)
+        
+
